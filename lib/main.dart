65a481d55b55676977908207456f738e8b21b9aa
PedroHSiqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2024-03-09 17:33:26 -0300

    Flutter app Start

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(new QuestionsApp());
+
+@override
+class QuestionsApp extends StatelessWidget{
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: Text("Olá Flutter!"),
+    );
+  }
+}
