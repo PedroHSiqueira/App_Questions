a564f90d1469c54548d11770ffaa1a739e930fb4
PedroHSiqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2024-03-17 00:03:31 -0300

    Scaffold and functions

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,45 @@
 import 'package:flutter/material.dart';
 
-main() => runApp(new QuestionsApp());
+void main() {
+  runApp(PerguntaApp());
+}
 
-@override
-class QuestionsApp extends StatelessWidget{
-  Widget build(BuildContext context){
+class PerguntaApp extends StatelessWidget {
+  void responder() {
+    print("Pergunta Respondida");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?',
+    ];
+
     return MaterialApp(
-      home: Text("Olá Flutter!"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Perguntas'),
+          centerTitle: true,
+        ),
+        body: Column(
+          children: [
+            Text(perguntas[0]),
+            ElevatedButton(
+              onPressed: responder,
+              child: const Text('Resposta 1'),
+            ),
+            ElevatedButton(
+              onPressed: responder,
+              child: const Text('Resposta 2'),
+            ),
+            ElevatedButton(
+              onPressed: responder,
+              child: const Text('Resposta 3'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
