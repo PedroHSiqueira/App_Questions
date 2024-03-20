843f9b084d0d71856b33d2df9d94f73b229279b4
PedroHSiqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2024-03-19 22:43:05 -0300

    Change question text method

--- lib/main.dart
+++ lib/main.dart
@@ -16,22 +16,31 @@ class _PerguntaAppState extends State<PerguntaApp> {
   @override
   Widget build(BuildContext context) {
     final perguntas = [
-      'Qual é a sua cor favorita?',
-      'Qual é o seu animal favorito?',
+      {
+        'texto': 'Qual é a sua cor favorita?',
+        'respostas': ['Preto', 'Vermelho', 'Verde', 'Branco'],
+      },
+      {
+        'texto': 'Qual é o seu animal favorito?',
+        'respostas': ['Coelho', 'Cobra', 'Elefante', 'Leão'],
+      },
+      {
+        'texto': 'Qual é a sua marca de carro favorita?',
+        'respostas': ['Porshe', 'Ford', 'Chevrolet', 'Dodge'],
+      },
     ];
 
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Perguntas'),
-          centerTitle: true,
         ),
         body: Column(
           children: [
-            Questao(perguntas[_perguntaSelecionada]),
-            Resposta("Resposta 1", _responder),
-            Resposta("Resposta 2", _responder),
-            Resposta("Resposta 3", _responder),
+            Questao(perguntas[_perguntaSelecionada]['texto'] as String),
+            Resposta('Resposta 1', _responder),
+            Resposta('Resposta 2', _responder),
+            Resposta('Resposta 3', _responder),
           ],
         ),
       ),
