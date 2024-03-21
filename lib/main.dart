6665bc533dffaba4e40389239a7fd70f7e017b66
PedroHSiqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2024-03-21 17:10:09 -0300

    ADD more Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,52 @@
 import 'package:flutter/material.dart';
-import './questao.dart';
-import './resposta.dart';
+import './questionario.dart';
+import './resultado.dart';
 
 void main() => runApp(const PerguntaApp());
 
 class _PerguntaAppState extends State<PerguntaApp> {
   var _perguntaSelecionada = 0;
 
+  final _perguntas = const [
+    {
+      'texto': 'Qual é a sua cor favorita?',
+      'respostas': ['Preto', 'Vermelho', 'Verde', 'Branco'],
+    },
+    {
+      'texto': 'Qual é o seu animal favorito?',
+      'respostas': ['Coelho', 'Cobra', 'Elefante', 'Leão'],
+    },
+    {
+      'texto': 'Qual é a sua marca de carro favorita?',
+      'respostas': ['Porshe', 'Ford', 'Chevrolet', 'Dodge'],
+    },
+  ];
+
   void _responder() {
-    setState(() {
-      _perguntaSelecionada++;
-    });
+    if (temPerguntaSelecionada()) {
+      setState(() {
+        _perguntaSelecionada++;
+      });
+    }
+  }
+
+  bool temPerguntaSelecionada() {
+    return _perguntaSelecionada < _perguntas.length;
   }
 
   @override
   Widget build(BuildContext context) {
-    final perguntas = [
-      {
-        'texto': 'Qual é a sua cor favorita?',
-        'respostas': ['Preto', 'Vermelho', 'Verde', 'Branco'],
-      },
-      {
-        'texto': 'Qual é o seu animal favorito?',
-        'respostas': ['Coelho', 'Cobra', 'Elefante', 'Leão'],
-      },
-      {
-        'texto': 'Qual é a sua marca de carro favorita?',
-        'respostas': ['Porshe', 'Ford', 'Chevrolet', 'Dodge'],
-      },
-    ];
-
-    List<Widget> respostas = [];
-    for (String textoResp
-        in perguntas[_perguntaSelecionada]['respostas'] as List) {
-      respostas.add(Resposta(textoResp, _responder));
-    }
-
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Perguntas'),
-        ),
-        body: Column(
-          children: [
-            Questao(perguntas[_perguntaSelecionada]['texto'] as String),
-            ...respostas,
-          ],
-        ),
-      ),
+          appBar: AppBar(
+            title: const Text('Perguntas'),
+          ),
+          body: temPerguntaSelecionada()
+              ? Questionario(
+                  perguntas: _perguntas,
+                  perguntaSelecionada: _perguntaSelecionada,
+                  responder: _responder)
+              : const Resultado()),
     );
   }
 }
