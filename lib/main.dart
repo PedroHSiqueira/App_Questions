bfde2f52029ac2b1d56a589659ca7083262f13d2
PedroHSiqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2024-03-22 17:04:05 -0300

    A little Refactor

--- lib/main.dart
+++ lib/main.dart
@@ -6,41 +6,43 @@ void main() => runApp(const PerguntaApp());
 
 class _PerguntaAppState extends State<PerguntaApp> {
   var _perguntaSelecionada = 0;
+  var _pontuacaoTotal = 0;
 
   final _perguntas = const [
     {
       'texto': 'Qual é a sua cor favorita?',
       'respostas': [
-        {"texto": 'Preto', "nota": 10},
-        {"texto": 'Vermelho', "nota": 5},
-        {"texto": 'Verde', "nota": 3},
-        {"texto": 'Branco', "nota": 1},
+        {"texto": 'Preto', "pontuacao": 10},
+        {"texto": 'Vermelho', "pontuacao": 5},
+        {"texto": 'Verde', "pontuacao": 3},
+        {"texto": 'Branco', "pontuacao": 1},
       ],
     },
     {
       'texto': 'Qual é o seu animal favorito?',
       'respostas': [
-        {"texto": 'Coelho', "nota": 10},
-        {"texto": 'Cobra', "nota": 5},
-        {"texto": 'Cachorro', "nota": 3},
-        {"texto": 'Gato', "nota": 1},
+        {"texto": 'Coelho', "pontuacao": 10},
+        {"texto": 'Cobra', "pontuacao": 5},
+        {"texto": 'Cachorro', "pontuacao": 3},
+        {"texto": 'Gato', "pontuacao": 1},
       ],
     },
     {
       'texto': 'Qual é a sua marca de carro favorita?',
       'respostas': [
-        {"texto": 'Dodge', "nota": 10},
-        {"texto": 'Porshe', "nota": 5},
-        {"texto": 'Chevrolet', "nota": 3},
-        {"texto": 'Alfa Romeo', "nota": 1},
+        {"texto": 'Dodge', "pontuacao": 10},
+        {"texto": 'Porshe', "pontuacao": 5},
+        {"texto": 'Chevrolet', "pontuacao": 3},
+        {"texto": 'Alfa Romeo', "pontuacao": 1},
       ],
     },
   ];
 
-  void _responder() {
+  void _responder(int pontuacao) {
     if (temPerguntaSelecionada()) {
       setState(() {
         _perguntaSelecionada++;
+        _pontuacaoTotal += pontuacao;
       });
     }
   }
